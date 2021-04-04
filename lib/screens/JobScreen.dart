a5c1e9e3f700bb25a0b8390a0cd36b4f27e32e30
benlittlenz <benlittle53@gmail.com>
2021-04-04 15:57:10 +1200

    listing jobs from api

--- lib/screens/JobScreen.dart
+++ lib/screens/JobScreen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:timesheet_app/models/APIResponse.dart';
 import 'package:timesheet_app/models/Job.dart';
 import 'package:timesheet_app/services/JobService.dart';
 
 class JobScreen extends StatefulWidget {
-
   @override
   _JobScreenState createState() => _JobScreenState();
 }
@@ -12,30 +12,43 @@ class JobScreen extends StatefulWidget {
 class _JobScreenState extends State<JobScreen> {
   JobService get service => GetIt.I<JobService>();
 
-  List<Job> jobs = [];
+  APIResponse<List<Job>> _jobs;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
+    _fetchJobs();
+  }
 
-    jobs = service.getJobs();
+  _fetchJobs() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _jobs = await service.getJobs();
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("List of Jobs")
-      ),
-      body: ListView.separated(
-        separatorBuilder: (_, __) => Divider(height: 1, color: Colors.green),
-          itemBuilder: (_, index) {
-            return ListTile(
-                title: Text(jobs[index].ref),
-            );
-          },
-          itemCount: jobs.length,
-      ),
+      appBar: AppBar(title: Text("List of Jobs")),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              separatorBuilder: (_, __) =>
+                  Divider(height: 1, color: Colors.green),
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text(_jobs.data[index].ref),
+                );
+              },
+              itemCount: _jobs.data.length,
+            ),
     );
   }
 }
--- lib/services/JobService.dart
+++ lib/services/JobService.dart
@@ -4,38 +4,31 @@ import 'package:timesheet_app/models/Job.dart';
 import 'package:http/http.dart' as http;
 
 class JobService {
-  static const API_URL = 'localhost:8000';
+  static const API_URL = '10.0.2.2:8000';
   static const headers = {
     'Content-Type': 'application/json'
   };
 
-  List<Job> getJobs() {
-    return [
-      new Job(
-        id: "1",
-        ref: "abc1",
-        description: "test 1",
-        address: "abc street",
-        suburb: "abc suburb",
-        city: "Auckland"
-      ),
-      new Job(
-        id: "2",
-        ref: "abc1",
-        description: "test 1",
-        address: "abc street",
-        suburb: "abc suburb",
-        city: "Auckland"
-      ),
-    ];
+  Future<APIResponse<List<Job>>> getJobs() {
+    return http.get(Uri.parse('http://10.0.2.2:8000/api/jobs'))
+      .then((res) {
+        if (res.statusCode == 200) {
+          final data = json.decode(res.body);
+          final jobs = <Job>[];
+
+          for(var item in data) {
+            final job = Job(
+              id: item['id'].toString(),
+              ref: item['job_ref']
+            );
+            jobs.add(job);
+          }
+          return APIResponse<List<Job>>(
+            data: jobs,
+          );
+        }
+        return APIResponse<List<Job>>(
+          error: true, errorMsg: "An error occured");
+      });
   }
-
-  // Future<APIResponse<List<Job>>> getJobs() {
-  //   return http.get(
-  //     Uri.http(API_URL, '/notes', headers: headers).then((data) {
-  //       final res = json.decode(data.body);
-
-  //     })
-  //   )
-  // }
 }
