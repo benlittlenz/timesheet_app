682572c47c89546cda70dd4e75e24e95dbe907bf
benlittlenz <benlittle53@gmail.com>
2021-04-10 19:23:00 +1200

    fetching token from db

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart' as Dio;
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:platform_device_id/platform_device_id.dart';
 import 'package:timesheet_app/models/User.dart';
+
+import '../dio.dart';
 
 class Auth extends ChangeNotifier {
   final _storage = FlutterSecureStorage();
@@ -11,11 +16,21 @@ class Auth extends ChangeNotifier {
   bool get authenticated => _isAuthenticated;
   User get user => _user;
 
-  Future login({Map credentials}) async {}
+  Future login({Map credentials}) async {
+    String deviceId = await getDeviceId();
+
+    Dio.Response response = await dio().post('auth/token',
+        data: json.encode(credentials..addAll({'deviceId': deviceId}))
+        //data: json.encode(credentials)
+        );
+    String token = json.decode(response.toString())['token'];
+
+    debugPrint(token);
+  }
 
   void logout() async {}
 
-    Future getDeviceId () async {
+  Future getDeviceId() async {
     String deviceId;
 
     try {
